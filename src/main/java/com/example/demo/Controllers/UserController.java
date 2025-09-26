3c720c5eb44ec6e078e077396716946b02007f4a
Obafemi <dadaobafemi26@gmail.com>
2025-09-26 10:18:30 +0100

    created entity, repository and controller

--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,10 +1,37 @@
 package com.example.demo.Controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entities.User;
+import com.example.demo.Repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getUser(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser( @RequestBody User user ) {
+        return userRepository.save(user);
+    }
+
+    
+
 }

57b6c1dc4f33e1c20c697cd8ea3d1de20da7413c
Obafemi <dadaobafemi26@gmail.com>
2025-09-26 09:39:43 +0100

    Initialized project

---
+++ src/main/java/com/example/demo/Entities/User.java
@@ -0,0 +1,154 @@
+package com.example.demo.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable =  false, updatable = false)
+    private String name;
+
+    @Column(name = "course", nullable =  false, updatable = false)
+    private String course;
+
+    @Column(name = "email", nullable =  false, updatable = false)
+    private String email;
+
+    @Column(name = "address", nullable =  false, updatable = false)
+    private String homeAddress;
+
+    @Column(name = "password", nullable =  false, updatable = false)
+    private String password;
+
+    public User(){}
+
+    public User(Long id, String name, String course, String email, String homeAddress, String password) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+        this.email = email;
+        this.homeAddress = homeAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((course == null) ? 0 : course.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((homeAddress == null) ? 0 : homeAddress.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (course == null) {
+            if (other.course != null)
+                return false;
+        } else if (!course.equals(other.course))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (homeAddress == null) {
+            if (other.homeAddress != null)
+                return false;
+        } else if (!homeAddress.equals(other.homeAddress))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", course=" + course + ", email=" + email + ", homeAddress="
+                + homeAddress + ", password=" + password + "]";
+    }
+
+    
+    
+}
